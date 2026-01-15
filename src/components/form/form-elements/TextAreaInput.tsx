d3ee4ca76d9fbeae7dfca494b13d61f14d7bd7b3
Caio Santos <caiosantos.dev@outlook.com>
2026-01-15 20:02:39 -0300

    fix

--- src/components/form/form-elements/TextAreaInput.tsx
+++ src/components/form/form-elements/TextAreaInput.tsx
@@ -9,35 +9,7 @@ export default function TextAreaInput() {
   const [messageTwo, setMessageTwo] = useState("");
   return (
     <ComponentCard title="Textarea input field">
-      <div className="space-y-6">
-        {/* Default TextArea */}
-        <div>
-          <Label>Description</Label>
-          <TextArea
-            value={message}
-            onChange={(value) => setMessage(value)}
-            rows={6}
-          />
-        </div>
-
-        {/* Disabled TextArea */}
-        <div>
-          <Label>Description</Label>
-          <TextArea rows={6} disabled />
-        </div>
-
-        {/* Error TextArea */}
-        <div>
-          <Label>Description</Label>
-          <TextArea
-            rows={6}
-            value={messageTwo}
-            error
-            onChange={(value) => setMessageTwo(value)}
-            hint="Please enter a valid message."
-          />
-        </div>
-      </div>
+      <></>
     </ComponentCard>
   );
 }
