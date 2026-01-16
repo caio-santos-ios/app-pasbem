398efdb11d9838b6afddc3dff433200db758028d
Caio Santos <caiosantos.dev@outlook.com>
2026-01-16 06:41:22 -0300

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,12 +6,22 @@ import { ThemeProvider } from '@/context/ThemeContext';
 import { Autorization } from '@/components/autorization/Autorization';
 import { Loading } from '@/components/loading/Loading';
 import { Bounce, ToastContainer } from 'react-toastify';
+import { Metadata, Viewport } from 'next';
 
 const outfit = Outfit({
   subsets: ["latin"],
 });
+// 1. O Objeto Viewport é CRUCIAL para remover a barra branca no Android e iOS
+export const viewport: Viewport = {
+  themeColor: "#ffffff",
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+  viewportFit: "cover", // Garante que o app use toda a tela (especialmente em iPhones com notch)
+};
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "PasBem",
   description: "Seu app de saúde",
   manifest: "/aplicativo/manifest.json",
@@ -32,6 +42,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-Br">
+      <head>
+        {/* Força o modo standalone em navegadores mobile legados */}
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
       <body className={`${outfit.className} dark:bg-gray-900`}>
         <ThemeProvider>
           {/* <Autorization /> */}
